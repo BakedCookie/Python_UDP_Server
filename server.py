2ae3906e3e8cf00d5c23d12844f4dd8c3beadaa7
Simon <ss4484@drexel.edu>
2019-10-22 14:56:17 -0400

    now sends min max values

--- server.py
+++ server.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python
 
-import random
 import socket
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_socket.bind(('', 22222))
 
+print("Hello clients, send your array of integers of any dimension to UDP Server Port No 22222")
+
 while True:
     message, address = server_socket.recvfrom(1024)
+
+    message = list(message)
+    message = [min(message),max(message)]
+
+    message = bytearray(message)
     server_socket.sendto(message, address)
