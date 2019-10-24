30fee8af27e3bbafc65c97aa42d21de85238b62e
Simon <ss4484@drexel.edu>
2019-10-24 09:42:11 -0400

    added printouts

--- client.py
+++ client.py
@@ -1,24 +1,37 @@
 #!/usr/bin/python
-
 import socket
 import random
 
+serverIP = 'localHost'
+serverPort = 22222
+bufferSize = 2048
+
+
 def makeList():
     message = []
-    for i in range(random.randint(5,20)):
-        message.append(random.randint(0,10))
+    for i in range(random.randint(5, 20)):
+        message.append(random.randint(0, 10))
     return message
 
-for pings in range(10):
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    client_socket.settimeout(1.0)
-    message_org = makeList()
-    message = bytearray(message_org)
-    addr = ("127.0.0.1", 22222)
-    client_socket.sendto(message, addr)
-    try:
-        data, server = client_socket.recvfrom(1024)
-        print(list(data))
-        print(message_org)
-    except socket.timeout:
-        print('REQUEST TIMED OUT')
+
+# Create a socket object for the client
+# AF_INET indicates IPv4 network, SOCK_DGRAM indicates UDP socket
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Create, encode and send message
+message_org = makeList()
+clientMessage = bytearray(message_org)
+clientSocket.sendto(clientMessage, (serverIP, serverPort))
+
+# We receive two parts
+serverData, serverAdd = clientSocket.recvfrom(bufferSize)
+
+print("Received new data from server")
+# Decode
+serverData = list(serverData)
+# Print to terminal
+print("From "+str(serverAdd[0])+" at "+str(serverAdd[1])+":")
+print("Min:"+str(serverData[0])+", Max:"+str(serverData[1]))
+
+clientSocket.close()
+
--- server.py
+++ server.py
@@ -1,17 +1,30 @@
 #!/usr/bin/python
-
 import socket
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_socket.bind(('', 22222))
+# Loop back IP, used for testing
+serverIP = 'localhost'
+serverPort = 22222
+bufferSize = 2048
 
+"""create a server socket object,
+typically takes 2 params, 1st defines IP type, 2nd is for protocol (UDP)"""
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# bind object to the address we created
+serverSocket.bind((serverIP, serverPort))
+
+# debug message
 print("Hello clients, send your array of integers of any dimension to UDP Server Port No 22222")
 
+# infinite loop
 while True:
-    message, address = server_socket.recvfrom(1024)
+    # listening for packets send from client
+    clientData, clientAdd = serverSocket.recvfrom(bufferSize)
+    # convert byte message back to array of ints
+    clientData = list(clientData)
+    clientData = [min(clientData), max(clientData)]
+    # convert message to byte message
+    clientData = bytearray(clientData)
+    # Send response to client
+    serverSocket.sendto(clientData, clientAdd)
 
-    message = list(message)
-    message = [min(message),max(message)]
-
-    message = bytearray(message)
-    server_socket.sendto(message, address)
